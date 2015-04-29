7e6d3e61eaf86b46232665a281b3fc7eb7705108
Masatoshi TAKAMIYA <gate000jp@gmail.com>
2015-04-29 23:39:14 +0900

    revert

--- jarfinder.bat
+++ jarfinder.bat
@@ -1,10 +1,9 @@
 @echo off
 setlocal
 
-rem if "%JAVA_HOME%"=="" (
-rem   set JAVA_HOME="c:\Program Files (x86)\Java\jdk1.8.0_25"
-rem )
+if "%JAVA_HOME%"=="" (
+  set JAVA_HOME="c:\Program Files (x86)\Java\jdk1.8.0_25"
+)
 
 "%JAVA_HOME%"\bin\java.exe -jar jarfinder.jar %*
-rem %JAVA_HOME%\bin\java.exe -cp bin jp.hashiwa.jarfinder.JarFinderMain %*
 
